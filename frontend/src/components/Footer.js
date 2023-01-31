d71489a79bdf51030b95cfcf4c50806537de4623
lauryn-21 <100782561+lauryn-21@users.noreply.github.com>
2023-01-31 09:18:13 +0300

    installed django framework

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -8,7 +8,7 @@ function Footer() {
     <footer>
       <Container>
         <Row>
-          <Col className="text-center py-3">Copyright &copy; ProShop</Col>
+          <Col className="text-center py-3">Copyright &copy; Lauryn</Col>
         </Row>
       </Container>
     </footer>
